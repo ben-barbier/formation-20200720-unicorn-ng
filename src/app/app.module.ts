38167e66e087ca28c8ab1cf24582570220f917b4
Benjamin Barbier <benjamin.barbier.59@gmail.com>
2020-07-20 17:48:18 +0200

    :sparkles: add @angular/material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,33 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from "@angular/common/http";
 import { UnicornListComponent } from './unicorn-list/unicorn-list.component';
 import { UnicornCardComponent } from './unicorn-list/unicorn-card/unicorn-card.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { NavComponent } from "./shared/components/nav/nav.component";
 
 @NgModule({
     declarations: [
         AppComponent,
         UnicornListComponent,
-        UnicornCardComponent
+        UnicornCardComponent,
+        NavComponent
     ],
     imports: [
         BrowserModule,
         AppRoutingModule,
         HttpClientModule,
+        BrowserAnimationsModule,
+        MatButtonModule,
+        MatIconModule,
+        LayoutModule,
+        MatToolbarModule,
+        MatSidenavModule,
+        MatListModule,
     ],
     providers: [],
     bootstrap: [AppComponent]
